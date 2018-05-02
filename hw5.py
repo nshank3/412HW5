b3e77038dbc4ae197aee0988a07c0aed03816829
nshank3 <nshank3@uic.edu>
2018-05-02 13:37:21 -0500

    Spelling errors

---
+++ hw5.py
@@ -0,0 +1,9 @@
+##from keras.models import Sequential
+##from keras.layers import Dense
+import numpy
+numpy.random.seed(8)
+#Loading Data#
+dataset = numpy.loadtxt("responses_stripped.csv", delimiter=",")
+#split into input(X) and output(Y) variables
+X = dataset[1:8, 141]
+print(X)
