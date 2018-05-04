7fb9344ed93b99600d458cace9217eded93909a5
nshank3 <nshank3@uic.edu>
2018-05-04 14:55:48 -0500

    Changed to Naive Bayes

--- hw5.py
+++ hw5.py
@@ -1,9 +1,10 @@
 ##from keras.models import Sequential
 ##from keras.layers import Dense
-import numpy
-numpy.random.seed(8)
-#Loading Data#
-dataset = numpy.loadtxt("responses_cleaned2.csv", delimiter=",")
-#split into input(X) and output(Y) variables
-X = dataset[1:8, 141]
-print(X)
+import numpy as np
+from sklearn import GaussianNB
+def Naive_Bayes():
+    print("Loading Data")
+    dataset = numpy.loadtxt("responses_cleaned2.csv", delimiter=",")
+    #split into input(X) and output(Y) variables
+    X = dataset
+Naive_Bayes()
